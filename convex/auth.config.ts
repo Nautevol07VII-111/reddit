2ee7b9323903e01c9ec23eb28f620b052950a6ea
Nautevol07VII-111 <nilejack@udel.edu>
2025-01-09 09:58:29 -0500

    Feat Adds: domain change

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        domain: "https://eminent-man-19.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]

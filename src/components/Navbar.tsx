9e315f6de62bfddc2658ec3e506b40c495acee66
Nautevol07VII-111 <nilejack@udel.edu>
2025-01-12 11:08:16 -0500

    Feat adds: tentative changes to various files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+        <div className="navbar-content">
+          <a href="/" className="logo">
+           Reddit Clone
+          </a>
+        </div>
+      </nav>
+    );  
+};
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
+import "./index.css";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
